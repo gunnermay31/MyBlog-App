3da44ca02f435fe4b2b6c06ea5588787883fde66
ankur <68180162+gunnermay31@users.noreply.github.com>
2024-02-12 16:17:19 +0530

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -1,20 +1,21 @@
-// blog.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// Import your controller
-const { dummyLink } = require('../controller/LikeController');
-const { createComment } = require('../controller/commentController');
-const { createPost,getAllPost } = require('../controller/postController');
 
-//creating mapping
-router.get('/dummyroute',dummyLink);
-router.post('/comments/create',createComment);
-router.post('/post/create',createPost);
-router.post('/getallposts',getAllPost);
+//Import Controller
+const {dummyLink,likepost,unlikePost} = require("../controller/LikeController");
+const {createComment} = require("../controller/commentController");
+const {createPost,GetAllPosts} = require("../controller/postController");
 
+
+//Mapping Create
+router.get("/dummyroute", dummyLink);
+router.post("/comments/create", createComment);
+router.post("/posts/create", createPost);
+router.get("/posts", GetAllPosts);
+router.post("/likes/like",likepost);
+router.post("/like/unlike",unlikePost);
 
 
 //export
-
 module.exports = router;
